08ff1cf49de97d4ef95cd1c2dc4bbff680598379
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-13 16:36:13 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+// const firstFunction = function(compass, item){
+//     console.log('The ' + compass,'wall has been painted '+ item);
+
+// };
+
+const firstFunction = function(item, compass){
+    console.log('The ' + compass,'wall has been painted '+ item);
+
+};
+
+// firstFunction ('north','orange');
+// firstFunction ('south-east','grey');
+
+
+firstFunction ('orange', 'north');
+firstFunction ('grey','south-east');
